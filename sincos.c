e3607a518b483168722db76c44524b800841ecad
Caya91 <marti.carnein@hotmail.de>
2023-10-10 11:31:35 +0200

    try outstashing

--- sincos.c
+++ sincos.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(int argc, char *argv[]){
-	printf("Anzahl der Argumente: %d", argc);
-	printf("%c", *argv[0]);
+	printf("Anzahl der Argumente: %d\n", argc);
+	printf("Argument 1: %s\n", argv[0]);
 	
 	char steps = *argv[1];
-	printf("%c\n", steps);
+	printf("Argument 2: %c\n", steps);
 	return 0;
 
 }
+
